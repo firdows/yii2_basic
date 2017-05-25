c1aabb5d67163b9391805a9abed5b197506ced70
Jehdu Ahamad <ahamad.jedu@gmail.com>
2017-05-26 00:30:52 +0700

    Update web.php

--- config/web.php
+++ config/web.php
@@ -11,7 +11,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'tmVaprDtnx9Ezny9d2oVPXb9DHSn-Lrm',
-            'baseUrl'=>'/yii2_basic'
+            'baseUrl'=>''
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
