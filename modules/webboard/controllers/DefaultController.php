2b45020a36e31af71094517d75acdd1461f5374a
Jehdu Ahamad <ahamad.jedu@gmail.com>
2017-05-25 17:02:56 +0700

    ปรับแต่งหน้า webboard

--- modules/webboard/controllers/DefaultController.php
+++ modules/webboard/controllers/DefaultController.php
@@ -2,19 +2,160 @@
 
 namespace app\modules\webboard\controllers;
 
+use Yii;
+use app\modules\webboard\models\Webboard;
+use app\modules\webboard\models\WebboardComment;
+use app\modules\webboard\models\WebboardSeach;
+use app\modules\webboard\models\WebboardCommentSeach;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+#ดัก action
+use yii\filters\AccessControl;
 
 /**
- * Default controller for the `webboard` module
+ * DefaultController implements the CRUD actions for Webboard model.
  */
 class DefaultController extends Controller
 {
     /**
-     * Renders the index view for the module
-     * @return string
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+          'access' => [
+              'class' => AccessControl::className(),
+              'rules' => [
+                  [
+                      'actions' => ['index',  'view'],
+                      'allow' => true,  #อนุญาติเข้า
+                  ],
+                  [
+                      'actions' => ['create','update','delete'],
+                      'allow' => true,
+                      'roles' => ['@'], # @ ต้องลงชื่อเข้าใช้
+                  ],
+              ],
+          ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Webboard models.
+     * @return mixed
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $searchModel = new WebboardSeach();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Webboard model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+
+        $searchModel = new WebboardCommentSeach(['webboard_id'=>$id]);
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+
+        $newComment = new WebboardComment(['webboard_id'=>$id]);
+        if ($newComment->load(Yii::$app->request->post()) && $newComment->save()) {
+            return $this->redirect(['view', 'id' => $id]);
+        }
+
+        $model =$this->findModel($id);
+        $model->updateCounters(['view' => 1]);
+
+        return $this->render('view', [
+            'model' => $model,
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+            'newComment'=>$newComment
+        ]);
+    }
+
+    /**
+     * Creates a new Webboard model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Webboard();
+        $model->setScenario('insert');
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing Webboard model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+        $model->setScenario('update');
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Webboard model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Webboard model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Webboard the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Webboard::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
     }
 }
--- modules/webboard/views/default/index.php
+++ modules/webboard/views/default/index.php
@@ -1,12 +1,54 @@
-<div class="webboard-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use app\modules\webboard\models\WebboardCategory;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\modules\webboard\models\WebboardSeach */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Webboards';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="webboard-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    
     <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
+        <?= Html::a('ตั้งกระทู้', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            //'id',
+            //'topic',
+            [
+              'attribute' => 'topic',
+              'format' => 'raw',
+              //'filter' => WebboardCategory::getList(),
+              'value'=> function($model){
+                return Html::a($model->topic,['view','id'=>$model->id])."<br/>";
+              }
+            ],
+            //'detail:ntext',
+            //'webboard_category_id',
+            [
+              'attribute' => 'webboard_category_id',
+              'filter' => WebboardCategory::getList(),
+              'value'=> 'webboardCategory.title'
+            ],
+            //'user_id',
+            // 'file',
+             'view',
+             'date_post:datetime',
+
+            //['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
 </div>
