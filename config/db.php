afed1fda9354df7166f2e5eb2c16cecb848e31f4
Firdows Ahmad <ahamad.jedu@gmail.com>
2017-05-26 22:42:47 +0700

    update db

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    //'dsn' => 'mysql:host=localhost;dbname=yii2_basic_db',
-    'dsn' => 'mysql:host=us-cdbr-iron-east-03.cleardb.net;dbname=heroku_59c6b9cd1f1cce7',
-    'username' => 'b263b69543d592',
+    'dsn' => 'mysql:host=localhost;dbname=yii2_basic_db',
+    'username' => 'root_d',
+    'password' => '',
     'charset' => 'utf8',
 ];
